713e1dde30452949401ee692c87b591d9dec1bf7
DevStar999 <wolverinerage99@gmail.com>
2021-08-15 03:50:46 +0530

    Adding a custom toolbar as the top action bar to the project

--- app/src/main/java/com/example/mdnavigationdrawersectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdnavigationdrawersectionproject/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.mdnavigationdrawersectionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    private void initialise() {
+        toolbar = findViewById(R.id.toolbar);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
+        setSupportActionBar(toolbar);
     }
 }
