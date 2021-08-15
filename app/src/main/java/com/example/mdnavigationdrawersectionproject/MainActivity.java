f17e2b43cc6c4c8d5589da0a8ce7ed13d4177963
DevStar999 <wolverinerage99@gmail.com>
2021-08-15 09:16:34 +0530

    Layout setup completed for the navigation drawer

--- app/src/main/java/com/example/mdnavigationdrawersectionproject/MainActivity.java
+++ app/src/main/java/com/example/mdnavigationdrawersectionproject/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.mdnavigationdrawersectionproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     private void initialise() {
         toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
 
         initialise();
         setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
     }
 }
